a9247294f08f15d734f758ce08afa7110d46725e
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2022-12-04 16:12:23 +0700

    hamdani

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'hamdani-porto';
+
+  options: AnimationOptions = {
+    path: '../assets/lottie/energy.json', // download the JSON version of animation in your project directory and add the path to it like ./assets/animations/example.json
+  };
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
+  
+  ngOnInit(): void {
+   
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
 import { AppComponent } from './app.component';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+export function playerFactory() {
+  return player;
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LottieModule.forRoot({ player: playerFactory }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
